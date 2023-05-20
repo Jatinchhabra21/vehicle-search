8a6c6d82523e2767d0dddbed9f13489165fd8485
Jatin Chhabra <cjatin2822@gmail.com>
2023-05-21 00:43:59 +0530

    handled error responses and created custom exception for manufacturer not found

---
+++ src/main/java/com/codecohort/vehicle/api/search/exception/ManufacturerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.codecohort.vehicle.api.search.exception;
+
+public class ManufacturerNotFoundException extends Exception {
+
+    public ManufacturerNotFoundException(String message) {
+        super(message);
+    }
+
+}
